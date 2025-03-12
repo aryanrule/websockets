724e773739926e788792b22847c71ecf5d68d3dd
Aryan sharma <aryansharma99107@gmail.com>
2025-03-12 23:48:08 +0530

    core logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,70 @@
-import express  , {Request , Response} from 'express';
+import express , {Request , Response} from 'express';
+import {Server} from 'socket.io';
+import { createServer } from 'http';
+import cors from 'cors';
 
-const httpServer = express();
 
-httpServer.get('/',(req:Request , res:Response) => {
-  res.send("helllo");  
+const port = 3000;
+const app = express();
+const httpserver = createServer(app);
+// reason to atach my httpserver from node js and app from express is to handle both HTTP requests and WebSocket connections using the same server.
+
+app.use(
+    cors(
+        {
+        origin : 'http://localhost:5173/'
+        }
+    )
+)
+
+// socket also need this cross origin resourse sharing
+const io = new Server(httpserver, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+      credentials: true , 
+    },
+  });
+  
+
+
+
+// io is a whole circuit but the socket is a client for understanding 
+io.on('connection' , (socket) => {
+    // console.log("A user Connected with a socket id" , socket);
+
+    console.log("user is connected" , socket.id);
+    socket.on("message" , (data) => {
+        io.emit("message" , data); // broadcasting to all the clients if you dont use the broadcast keyword
+    })
+    
+    socket.on("sending-message" , (data) => {
+        // socket.broadcast.emit("sending-message" , data);
+        
+        // socket.broadcast.emit("sending-message" , data.input); // entire circuit expcept the current socket
+        socket.to(data.room).emit("sending-message" , data.input);
+    })
+   
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+
+    socket.emit("welcome" , `welcome to the server ${socket.id}`);
+    
+    // broadcast event will send this accept this socket id
+    socket.broadcast.emit("welcome" , `welcome to the server this is a broadcasting event , ${socket.id}`);
+    
+    
 })
 
 
-httpServer.listen(3000 , ()=>{
-    console.log("httpserver is connected");
-}) 
+
+app.get('/' , (req : Request , res : Response) => {
+    res.send(`<h1>hello world </h1>`);
+});
+
+httpserver.listen(port, () => {
+    console.log("server is connected successfully");
+})
+
+
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,71 @@
+import { useEffect, useState  , useMemo, useRef } from 'react';
 import './App.css'
-
+import {io} from 'socket.io-client';
 //  creating a simple chat app to understand websockets 
 
 function App() {
-  // how client works with websocket  
+  // how client works with websocket
+  const socket = useMemo(() =>io('http://localhost:3000'), []);
+  const [input , setInput] = useState<String>("");
+  const [reciveddata , setRecivedData] = useState<String>("");
+  const [socketId , setSocketId] = useState<String | undefined>("");
+  const [room , setRoom] = useState<string>("");
+  
+
+  const submitHandler = (event : any)=>{
+      
+        event?.preventDefault();
+        socket.emit("sending-message" , {input , room});
+        setInput("");
+        setRoom("");
+
+  }
+  useEffect(() => {
+    socket.on('connect' , ()=> {
+       setSocketId(socket.id);
+       console.log("connected with "  , socket.id);
+    })
+
+    socket.on("sending-message" , (data) => {
+      console.log("recieved message" ,  data);
+      setRecivedData(data);
+    })
+   
+    socket.on("welcome" , (data) => {
+        console.log(data);
+    })
+    console.log(input);
+    console.log(room);
+  } , [input , socket , room]);
+
+
 
   return (
+
     <>
       <div> 
-            <input placeholder='enter your message'/>
-            <button>send</button>
-            <p>here is the recived message below :</p>
-            <p></p> 
+            <h1>{socketId}</h1>
+          
+            <form onSubmit={submitHandler}>
+                 <input 
+                 
+                required= {true}
+                 onChange={(event)=>{setInput(event.target.value)}}
+                 placeholder='enter the message'></input>
+
+                 <input
+                  placeholder='please give me room'
+                  onChange={(event)=>{setRoom(event.target.value)}}
+                  >
+                    
+                 </input>
+                 <button type='submit'>send</button>
+            </form>
+
+
+
+            <p>here is the recived message</p>
+            <p>{reciveddata}</p>
       </div>
     </>
   )
